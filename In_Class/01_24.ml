16d9e4bbb6d78036161380c4cca19672527cd464
vetru005 <vetru005@umn.edu>
2024-01-24 14:15:32 -0600

    Update 01_24.ml

--- In_Class/01_24.ml
+++ In_Class/01_24.ml
@@ -1,3 +1,6 @@
 let cylinder_volume r h = let pi = 3.14159265389793 in r *. r *. pi *. h
 
-let power (e: int) (term: float) : float = if (e == 0) : 1 else if (e == 1) : term else : power term (e - 1) *. term
+let power (e: int) (term: float) : float =
+  if (e == 0) then 1
+  else if (e == 1) then term
+  else term *. power (e - 1) term
