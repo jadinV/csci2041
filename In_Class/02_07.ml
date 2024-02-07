284cb1197667305b45b567065992e90063e7e457
Jadin Vetruba <vetru005@umn.edu>
2024-02-07 14:13:05 -0600

    Done in class

---
+++ In_Class/02_07.ml
@@ -0,0 +1,8 @@
+(*
+  Logical errors: Found at runtime
+  Syntax errors: Found by compiler
+  Format errors (not giving the right number or kind of parameters to a function): Found by compiler
+  Memory errors: Found at runtime
+  Time-out errors: Found at runtime
+*)
+
