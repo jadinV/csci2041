6c8e5df01dd46b49704bb82825c27a9cbe7f5e68
Jadin Vetruba <vetru005@umn.edu>
2024-02-20 11:17:02 -0600

    Start of lab 6

---
+++ Lab_06/src/lab_06.mli
@@ -0,0 +1,2 @@
+val list_sum : int list -> int
+val list_add : int -> int list -> int list
---
+++ Lab_06/tests/lab_06_tests.ml
@@ -0,0 +1,4 @@
+(* This makes the functions declared in lab_06.mli visible. *)
+open Lab_06
+
+let () = assert (list_sum [] = 0)
