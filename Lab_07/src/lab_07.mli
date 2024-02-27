13db49d94cf420c837f142a5c4c9bf799a8590ac
Jadin Vetruba <vetru005@umn.edu>
2024-02-27 11:20:49 -0600

    Started lab 7

---
+++ Lab_07/src/lab_07.mli
@@ -0,0 +1,3 @@
+val squared_list : int list -> int list
+val list_evens : int list -> int list
+val list_product : int list -> int
---
+++ Lab_07/tests/lab_07_tests.ml
@@ -0,0 +1,4 @@
+(* This makes the functions declared in lab_07.mli visible. *)
+open Lab_07
+
+let () = assert (squared_list [] = [])
