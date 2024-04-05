4f2a5a14514e28aabb7b4a14f317388557f1f9fc
Jadin Vetruba <vetru005@umn.edu>
2024-04-05 15:35:11 -0500

    Fixed issues with treemap

--- Hwk_04/literacy/src/mapImpl.ml
+++ Hwk_04/literacy/src/mapImpl.ml
@@ -2,14 +2,17 @@ module ListMapImpl = struct
   type ('k,'v) t = ('k * 'v) list
 
   let empty = []
+
   let lookup k lst = match List.find_opt (fun (x, _) -> x = k) lst with
                      | None -> None
                      | Some (_, v) -> Some v
+  
   let insert k v lst = if (List.exists (fun (x, _) -> x = k) lst)
                           then List.map (fun (x, cv) -> if x = k
                                                         then (k, v)
                                                         else (x, cv)) lst
                           else (k, v)::lst
+  
   let remove k lst = List.filter_map (fun (x, cv) -> if (x = k)
                                                           then None
                                                           else Some (x, cv)) lst
@@ -20,29 +23,46 @@ module TreeMapImpl = struct
                   | Node of ('k, 'v) t * 'k * 'v * ('k, 'v) t
 
   let empty = Empty
-  let lookup k tr = let rec helper ky tre =
-                      match tre with
-                      | Empty -> None
-                      | Node (_, key, v, _) when key = ky -> Some v
-                      | Node (l, _, _, _) when helper ky l != None -> helper ky l
-                      | Node (_, _, _, r) -> helper ky r
-                    in helper k tr
-  let insert k v tr = let rec replace k v tr =
-                      match tr with
-                      | Empty -> Empty
-                      | Node (l, ky, _, r) when ky = k -> Node (l, k, v, r)
-                      | Node (l, ky, value, r) -> Node (replace k v l, ky, value, replace k v r)
-                      in match lookup k tr with
-                         | None -> Node (tr, k, v, Empty)
-                         | Some _ -> replace k v tr
-  let remove k tr = let rec shift_up ltr rtr =
-                      match rtr with
-                      | Empty -> Empty
-                      | Node (rrl, rrk, rrv, rrr) -> Node (ltr, rrk, rrv, shift_up rrl rrr)
-                    in let rec rmv k tr =
-                      match tr with
-                      | Empty -> Empty
-                      | Node (l, ky, _, r) when ky = k -> shift_up l r
-                      | Node (l, ky, v, r) -> Node (rmv k l, ky, v, rmv k r)
-                    in rmv k tr
+
+  let rec helper ky tre =
+    match tre with
+    | Empty -> None
+    | Node (_, key, v, _) when key = ky -> Some v
+    | Node (l, key, _, _) when key < ky -> helper ky l
+    | Node (_, _, _, r) -> helper ky r
+
+  let rec lookup k tr = 
+    match tr with
+    | Empty -> None
+    | Node (_, key, v, _) when key = k -> Some v
+    | Node (l, key, _, _) when key > k -> lookup k l
+    | Node (_, _, _, r) -> lookup k r
+
+  let rec add ky value tre =
+    match tre with
+    | Empty -> Node (Empty, ky, value, Empty)
+    | Node (l, key, _, r) when key = ky -> Node (l, ky, value, r)
+    | Node (l, key, v, r) when key < ky -> Node (l, key, v, add ky value r)
+    | Node (l, key, v, r) -> Node (add ky value l, key, v, r)
+
+  let insert k v tr = add k v tr
+
+  let rec tree_fix tr left =
+    match tr with
+    | Empty -> raise (Failure "Empty tree")
+    | Node (Empty, k, v, r) -> Node (left, k, v, r)
+    | Node (l, _, _, _) -> tree_fix l left
+
+  let rec remove k tr =
+    match tr with
+    | Empty -> raise (Failure ("No key with value k"))
+    | Node (_, ky, _, _) when k = ky -> (match tr with
+                                         | Empty -> raise (Failure "How did you even get here?")
+                                         | Node (Empty, _, _, Empty) -> Empty
+                                         | Node (l, _, _, Empty) -> l
+                                         | Node (Empty, _, _, r) -> r
+                                         | Node (l, _, _, r) -> tree_fix r l
+                                        )
+    | Node (l, ky, v, r) when k > ky -> Node (l, ky, v, remove k r)
+    | Node (l, ky, v, r) -> Node (remove k l, ky, v, r)
 end
