83404b7e4d0337ff080a450d095a25183a728bca
Jadin Vetruba <vetru005@umn.edu>
2024-04-19 00:13:34 -0500

    Finished warmup

--- Hwk_06/warmup/src/expr.ml
+++ Hwk_06/warmup/src/expr.ml
@@ -43,14 +43,66 @@ exception IncorrectType of string
 let rec eval (e:expr) (env: environment) : value =
   match e with
   | Val v -> v
-
   | Add (e1, e2) ->
-     ( match eval e1 env, eval e2 env with
-       | Int v1, Int v2 -> Int (v1 + v2)
-       | _ -> raise (IncorrectType "Add")
-     )
-
-  (* Remove this clause when you are finished implementing this function. *)
-  | _ -> raise (Failure "complete eval")
-
-
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 -> Int (v1 + v2)
+        | _ -> raise (IncorrectType "Add")
+      )
+  | Sub (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 -> Int (v1 - v2)
+        | _ -> raise (IncorrectType "Sub")
+      )
+  | Mul (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 -> Int (v1 * v2)
+        | _ -> raise (IncorrectType "Mul")
+      )
+  | Div (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 when v2 = 0 -> raise (DivisionByZero (Int v1))
+        | Int v1, Int v2 -> Int (v1 / v2)
+        | _ -> raise (IncorrectType "Div")
+      )
+  | Neg e1 ->
+      ( match eval e1 env with
+        | Int v1 -> Int ((-1) * v1)
+        | _ -> raise (IncorrectType "Neg")
+      )
+  | Lt (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 -> Bool (v1 < v2)
+        | _ -> raise (IncorrectType "Lt")
+      )
+  | Gt (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 -> Bool (v1 > v2)
+        | _ -> raise (IncorrectType "Gt")
+      )
+  | Eq (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Int v1, Int v2 -> Bool (v1 == v2)
+        | _ -> raise (IncorrectType "Eq")
+      )
+  | And (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Bool v1, Bool v2 -> Bool (v1 && v2)
+        | _ -> raise (IncorrectType "And")
+      )
+  | Or (e1, e2) ->
+      ( match eval e1 env, eval e2 env with
+        | Bool v1, Bool v2 -> Bool (v1 || v2)
+        | _ -> raise (IncorrectType "Or")
+      )
+  | Not e1 ->
+      ( match eval e1 env with
+        | Bool v1 -> Bool (not v1)
+        | _ -> raise (IncorrectType "Not")
+      )
+  | Let (str, e1, e2) -> eval e2 [(str, (eval e1 env))]
+  | Id str ->
+      ( match env with
+      | [] -> raise (UnboundVariable str)
+      | (s, v) :: _ when s = str -> v
+      | _ :: lst -> eval (Id str) lst
+      )
