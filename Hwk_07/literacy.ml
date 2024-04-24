493e3804f11789e4eeaadf9dbf68cea4371dab12
Jadin Vetruba <vetru005@umn.edu>
2024-04-24 14:11:50 -0500

    Finished warmup

--- Hwk_07/literacy.ml
+++ Hwk_07/literacy.ml
@@ -4,15 +4,26 @@ open Warmup
 
 let rec try_option (env_so_far: subst) (free_vars: string list) (f: formula) : 
           subst option =
-
-  (* The following code should be removed and replaced by your
-     implementation of this function.
-   *)
-  None
-
+  match f with
+  | Prop s -> Some (s, false)
+  | And (f1, f2) -> ( match (try_option env_so_far free_vars f1, try_option env_so_far free_vars f2) with
+                      | (Some one, Some two) -> combine_subst one two
+                      | (Some one, None) -> Some (one)
+                      | (None, Some two) -> Some (two)
+                      | _ -> None
+                    )
+  | Implies (f1, f2) -> None
+  | Or (f1, f2) -> ( match (try_option env_so_far free_vars f1, try_option env_so_far free_vars f2) with
+                     | (Some one, Some two) -> Some (one @ two)
+                     | _ -> None
+                   )
+  | Not f1 -> ( match (try_option env_so_far free_vars f1) with
+                | Some one -> Some (List.map (fun (str, b) -> (str, not b)) one)
+                | None -> None
+              )
+  | True -> None
+  | False -> Some env_so_far
 
 
 let tautology_checker_option (f:formula) : subst option =
   try_option [] (freevars f) f
-
-
